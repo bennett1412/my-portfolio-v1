52d755a3b45496ac6cd23828299690f16e39b8cd
Bennett B Madavana <bennettmadavana@gmail.com>
2021-07-15 20:14:07 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+// import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+function App() {
+  return (
+   <main className='text-gray-400 bg-gray-900 body-font' >
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Testimonials.js
@@ -0,0 +1,9 @@
+const Testimonials = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Testimonials
