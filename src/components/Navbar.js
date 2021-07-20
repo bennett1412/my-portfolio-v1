8b354aed45d4b063be48884daac01e74b8a1e944
Bennett B Madavana <34763279+bennett1412@users.noreply.github.com>
2021-07-20 20:41:01 +0530

    Customise project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,34 @@
+import { ArrowRightIcon } from "@heroicons/react/solid";
 const Navbar = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <header className="bg-gray-100 md:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a className="title-font font-medium text-gray-900 mb-4 md:mb-0">
+          <a href="#about" className="ml-3 text-xl">
+            Bennett
+          </a>
+        </a>
+        <nav className="text-gray-800 md:mr-auto md:ml-4 md:py-1 md:pl-3 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+          <a href="#projects" className="mr-5 hover:text-blue-900">
+            My Projects
+          </a>
+          <a href="#skills" className="mr-5 hover:text-blue-900">
+            Skills
+          </a>
+          {/* <a href="#testimonials" className="mr-5 hover:text-white">
+            Testimonials
+          </a> */}
+        </nav>
+        <a
+          href="#contact"
+          className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:shadow-md hover:bg-white rounded text-gray-900 mt-4 md:mt-0"
+        >
+          Connect with Me
+          <ArrowRightIcon className="w-4 h-4 ml-1" />
+        </a>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/data.js
+++ src/data.js
@@ -1,34 +1,18 @@
 export const projects = [
   {
-    title: "React Reserve",
-    subtitle: "MERN Stack",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    title: "React ToDo list",
+    subtitle: "",
+    description: "A simple todo list app",
     image: "./project-1.gif",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "React Tracks",
-    subtitle: "React and Python",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reedbarger.com",
-  },
-  {
-    title: "DevChat",
-    subtitle: "React and Firebase",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://jsbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://pythonbootcamp.com",
-  },
+    link: "https://reactbootcamp.com"
+  }
+];
+
+export const skills = [
+  "Python",
+  "Django",
+  "JavaScript",
+  "React",
+  "HTML/CSS",
+  "PHP"
 ];
