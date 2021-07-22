3a1702e01606d5419dcb04960817e28d8e842fd6
Bennett B Madavana <bennettmadavana@gmail.com>
2021-07-22 20:31:51 +0530

    Add styled-components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -53,9 +53,9 @@ const Contact = () => {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <div className="text-indigo-400 leading-relaxed">
                 bennettmadavana@gmail.com
-              </a>
+              </div>
               {/* <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
               </h2>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,23 @@
 import { ArrowRightIcon } from "@heroicons/react/solid";
+import styled from 'styled-components';
+const navLink = styled.a`
+  color:red;
+  &:hover{
+    color:red;
+  }
+`;
 const Navbar = () => {
+  
   return (
     <header className="bg-gray-100 md:sticky top-0 z-10">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a className="title-font font-medium text-gray-900 mb-4 md:mb-0">
+        <a href="#about" className="title-font font-medium text-gray-900 mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Bennett
           </a>
         </a>
         <nav className="text-gray-800 md:mr-auto md:ml-4 md:py-1 md:pl-3 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
-          <a href="#projects" className="mr-5 hover:text-blue-900">
-            My Projects
-          </a>
+         <navLink>hello</navLink>
           <a href="#skills" className="mr-5 hover:text-blue-900">
             Skills
           </a>
